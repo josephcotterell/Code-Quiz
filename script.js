df458e8eeb8c99e800f3d72cb088dc9b70e98183
joseph <josephcotterell@hotmail.co.uk>
2022-11-27 12:00:22 +0000

    added start button id to javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById('start-btn')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+
+    startButton.classList.add
+}
+
+function setNextQuestion() {}
+
+
+function selectAnswer{
+
+}
