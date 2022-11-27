50639d488fcf5eab0ddfd9235cac1d677e1d6bf6
joseph <josephcotterell@hotmail.co.uk>
2022-11-27 12:08:24 +0000

    added question container element

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const startButton = document.getElementById('start-btn')
+const questionContainerElement = document.getElementById ('question-container')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
 
-    startButton.classList.add
+    startButton.classList.add ('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {}
